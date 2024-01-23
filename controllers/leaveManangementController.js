cb792177102f7a2072092ff7793dc449ae151e5a
Sifiso <dr96mabuza@gmail.com>
2024-01-23 11:10:50 +0200

    leave update getLeaves only works

--- controllers/leaveManangementController.js
+++ controllers/leaveManangementController.js
@@ -1,7 +1,7 @@
 const connection = require("../database");
 
 exports.getLeaves = (req, res) => {
-    connection.query("select * from leaveManagement", (error, rows, fields) => {
+    connection.query("select * from leaves", (error, rows, fields) => {
         if (error) {
             res.json({status: "error", result: error});
         };
@@ -10,9 +10,9 @@ exports.getLeaves = (req, res) => {
 };
 
 exports.createLeave = (req, res, next) => {
-    const query = "insert into leaveManagement " +
-        "(leaveBalance, daysAbsent, employeeId) " +
-        `values( '${req.body.leaveBalance}', '${req.body.daysAbsent}',  ${req.body.employeeId})`;
+    const query = "insert into leaves " +
+    "INSERT INTO leaves (leave_type, reason, start_date, end_date, days_absent, employee_id, approval) "+
+        `values( '${req.body.leave_type}', '${req.body.reason}', '${req.body.start_date}', '${req.body.end_date}', ${req.body.employeeId}, '${req.body.approval}')`;
     connection.query(query, (error, results) => {
         if (error) res.json({status: "error", result: error});
         res.json({status: "ok", result: results});
@@ -20,7 +20,7 @@ exports.createLeave = (req, res, next) => {
 };
 
 exports.getLeave = (req, res, next) => {
-    connection.query(`select * from leaveManagement where id = ${req.params.id}`, (error, results) => {
+    connection.query(`select * from leaves where leave_id = ${req.params.id}`, (error, results) => {
         if (error) res.json({status: "error", result: error});
         res.json({status: "ok", result: results});
     });
@@ -28,10 +28,9 @@ exports.getLeave = (req, res, next) => {
 
 exports.updateLeave = (req, res, next) => {
     
-    const queryString = "update leaveManagement " +
-        `set leaveBalance=${req.body.leaveBalance}, ` + 
-        `daysAbsent=${req.body.daysAbsent}, ` + 
-        `employeeId=${req.body.employeeId} ` +
+    const queryString = "update leaves " +
+        `set (leave_type, reason, start_date, end_date, days_absent, approval)` +
+        `values( '${req.body.leave_type}', '${req.body.reason}', '${req.body.start_date}', '${req.body.end_date}', '${req.body.approval}'`;
         `where id = ${req.params.id}`;
     
     connection.query(queryString, (error, results) => {
@@ -41,7 +40,7 @@ exports.updateLeave = (req, res, next) => {
 };
 
 exports.deleteLeave = (req, res, next) => {
-    connection.query(`delete from leaveManagement where id = ${req.params.id}`, (error, results) => {
+    connection.query(`delete from leaves where id = ${req.params.id}`, (error, results) => {
         if (error) res.json({status: "error", result: error});
         res.json({status: "ok", result: results});
     });
